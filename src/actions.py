31a52c3a9aed693c08d5f8aafcde244d3cb6b4eb
Patrick <patrick.nikanti@swtech.fi>
2021-03-15 20:51:20 +0200

    update Actions.py. WIP

--- src/actions.py
+++ src/actions.py
@@ -1,22 +1,26 @@
-from config import Config
-from modem import HuaweiClient
-from influx import InfluxClient
-from ookla import OoklaClient
+from config import Configurator
 
 
-class Actions(Config):
+class Actions(Configurator):
     def __init__(self):
         super().__init__()
-        self.huawei = HuaweiClient()
-        self.influx = InfluxClient()
-        self.ookla = OoklaClient()
-        self.isp = self.ookla.get_isp()
-        self.ip = self.ookla.get_ip()
-        self.country = self.ookla.get_country()
-        self.cell_id = self.huawei.get_cell_id()
-        self.rsrq = self.huawei.get_rsrq()
-        self.rsrp = self.huawei.get_rsrp()
-        self.rssi = self.huawei.get_rssi()
+        if self.DATABASE_ENABLED:
+            self.database = self.get_database_client()
+        if self.SPEEDTEST_ENABLED:
+            self.speedtest = self.get_speedtest_client()
+        if self.MODEM_ENABLED:
+            self.modem = self.get_modem_client()
+
+        self.isp = self.speedtest.get_isp()
+        self.ip = self.speedtest.get_ip()
+        self.country = self.speedtest.get_country()
+        self.cell_id = self.modem.get_cell_id()
+
+    def measure_signal(self):
+        pass
+        # self.rsrq = self.modem.get_rsrq()
+        # self.rsrp = self.modem.get_rsrp()
+        # self.rssi = self.modem.get_rssi()
 
     def measure_download(self):
         tags = {
@@ -27,17 +31,14 @@ class Actions(Config):
             "cell_id": self.cell_id,
         }
         data = {
-            "pre_download_traffic": self.huawei.get_download_traffic(),
-            "download_bandwith": self.ookla.measure_download(),
+            "pre_download_traffic": self.modem.get_download_traffic(),
+            "download_bandwith": self.speedtest.measure_download(),
             "isp": self.isp,
             "ip": self.ip,
             "cell_id": self.cell_id,
-            "rsrq": self.rsrq,
-            "rsrp": self.rsrp,
-            "rssi": self.rssi,
         }
         self.logger.info(data)
-        self.influx.post(measurement="download_bandwith", data=data, tags=tags)
+        self.database.post(measurement="download_bandwith", data=data, tags=tags)
 
     def measure_upload(self):
         tags = {
@@ -48,17 +49,14 @@ class Actions(Config):
             "cell_id": self.cell_id,
         }
         data = {
-            "pre_upload_traffic": self.huawei.get_upload_traffic(),
-            "upload_bandwidth": self.ookla.measure_upload(),
+            "pre_upload_traffic": self.modem.get_upload_traffic(),
+            "upload_bandwidth": self.speedtest.measure_upload(),
             "isp": self.isp,
             "ip": self.ip,
             "cell_id": self.cell_id,
-            "rsrq": self.rsrq,
-            "rsrp": self.rsrp,
-            "rssi": self.rssi,
         }
         self.logger.info(data)
-        self.influx.post(measurement="upload_bandwith", data=data, tags=tags)
+        self.database.post(measurement="upload_bandwith", data=data, tags=tags)
 
 
 if __name__ == "__main__":
